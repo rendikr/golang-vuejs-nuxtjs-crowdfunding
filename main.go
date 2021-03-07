a5b97f444e53fa667447d89bcec32610a505ac4d
Rendi K <rendi.krinaldi@gmail.com>
2021-03-07 16:48:38 +0700

    user email check handler

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func main() {
 
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
+	api.POST("/email_check", userHandler.CheckEmailAvailability)
 
 	router.Run()
 }
