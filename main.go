a3df7e05c4bb852ef68d5bb593c35af484ef54b2
Rendi K <rendi.krinaldi@gmail.com>
2021-03-07 11:05:55 +0700

    gorm get table to json

--- main.go
+++ main.go
@@ -1,37 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"golang-crowdfunding-backend/user"
 	"log"
+	"net/http"
+	"os/user"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "root:password@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:ReviRzA89@!@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Database Connected!")
+
+	// var users []user.User
+
+	// db.Find(&users)
+
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// 	fmt.Println(user.Occupation)
+	// 	fmt.Println("---")
+	// }
+
+	router := gin.Default()
+	router.GET("/users", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
+	dsn := "root:ReviRzA89@!@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Database Connected!")
-
 	var users []user.User
-	length := len(users)
-	fmt.Println(length)
-
 	db.Find(&users)
 
-	length = len(users)
-	fmt.Println(length)
-
-	for _, user := range users {
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-		fmt.Println(user.Occupation)
-		fmt.Println("---")
-	}
+	c.JSON(http.StatusOK, users)
 }
