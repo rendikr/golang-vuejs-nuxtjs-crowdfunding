17771042b43e4eab9a5fe824d5d011ccf421b671
Rendi K <rendi.krinaldi@gmail.com>
2021-03-08 08:13:57 +0700

    create campaign service 1

--- campaign/input.go
+++ campaign/input.go
@@ -1,5 +1,16 @@
 package campaign
 
+import "golang-crowdfunding-backend/user"
+
 type GetCampaignDetailInput struct {
 	ID int `uri:"id" binding:"required"`
+}
+
+type CreateCampaignInput struct {
+	Name             string `json:"name" binding:"required"`
+	ShortDescription string `json:"short_description"`
+	Description      string `json:"description"`
+	GoalAmount       int    `json:"goal_amount" binding:"required"`
+	Perks            string `json:"perks"`
+	User             user.User
 }
