84fd0a901e46f73457d2528b22178360b055a994
Rendi K <rendi.krinaldi@gmail.com>
2021-03-08 13:31:25 +0700

    create transaction repository

--- transaction/repository.go
+++ transaction/repository.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type Repository interface {
 	GetByCampaignID(campaignID int) ([]Transaction, error)
 	GetByUserID(userID int) ([]Transaction, error)
+	Save(transaction Transaction) (Transaction, error)
 }
 
 type repository struct {
@@ -35,4 +36,13 @@ func (r *repository) GetByUserID(userID int) ([]Transaction, error) {
 	}
 
 	return transactions, nil
+}
+
+func (r *repository) Save(transaction Transaction) (Transaction, error) {
+	err := r.db.Create(&transaction).Error
+	if err != nil {
+		return transaction, err
+	}
+
+	return transaction, nil
 }
