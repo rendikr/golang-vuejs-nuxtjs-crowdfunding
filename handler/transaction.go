c0b005620dbf6c8e96485763f5f801009f54cb78
Rendi K <rendi.krinaldi@gmail.com>
2021-03-08 13:00:59 +0700

    user transaction handler

--- handler/transaction.go
+++ handler/transaction.go
@@ -41,3 +41,19 @@ func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
 	response := helper.APIResponse("Get campaign transactions success", http.StatusOK, "success", transaction.FormatCampaignTransactions(transactions))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *transactionHandler) GetUserTransactions(c *gin.Context) {
+	currentUser := c.MustGet("currentUser").(user.User)
+
+	userID := currentUser.ID
+
+	transactions, err := h.service.GetTransactionsByUserID(userID)
+	if err != nil {
+		response := helper.APIResponse("Get user transactions failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Get user transactions success", http.StatusOK, "success", transactions)
+	c.JSON(http.StatusOK, response)
+}
