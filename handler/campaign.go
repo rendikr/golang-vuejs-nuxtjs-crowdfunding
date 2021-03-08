ca535cf8941554192fe7a0d709d5322a5e1d4706
Rendi K <rendi.krinaldi@gmail.com>
2021-03-08 07:38:40 +0700

    get campaign detail handler

--- handler/campaign.go
+++ handler/campaign.go
@@ -30,3 +30,24 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	response := helper.APIResponse("Get campaigns success", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *campaignHandler) GetCampaign(c *gin.Context) {
+	var input campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Get campaign detail failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campaign, err := h.service.GetCampaignByID(input)
+	if err != nil {
+		response := helper.APIResponse("Get campaign detail failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Get campaign detail success", http.StatusOK, "success", campaign)
+	c.JSON(http.StatusOK, response)
+}
